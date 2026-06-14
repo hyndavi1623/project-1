903d7e89053b921835fe389fdfc2d00f9fae923b
hyndavi1623 <hyndavispk16@gmail.com>
2026-06-14 14:48:14 +0530

    Update database.sql

--- dbfile/database.sql
+++ dbfile/database.sql
@@ -1,34 +1,15 @@
-CREATE DATABASE instagram_clone;
+CREATE DATABASE employee_db;
 
-USE instagram_clone;
+USE employee_db;
 
-CREATE TABLE users (
+CREATE TABLE employees (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) UNIQUE NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    name VARCHAR(100) NOT NULL,
+    department VARCHAR(100),
+    salary DECIMAL(10,2)
 );
 
-CREATE TABLE posts (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT,
-    image_url TEXT,
-    caption TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (user_id)
-        REFERENCES users(id)
-        ON DELETE CASCADE
-);
-
-INSERT INTO users(username,email,password)
+INSERT INTO employees(name, department, salary)
 VALUES
-('john','john@gmail.com','12345');
-
-INSERT INTO posts(user_id,image_url,caption)
-VALUES
-(
-1,
-'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d',
-'My first post!'
-);
+('John Smith', 'IT', 55000),
+('Sarah Johnson', 'HR', 48000);
